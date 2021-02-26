56972c1a7e730fc50305751cf0f44f3d1082bf06
kel <kelpius21@gmail.com>
2021-02-26 09:31:15 -0500

    Updated serializers for locations.

--- covidapp_django/covidapp/urls.py
+++ covidapp_django/covidapp/urls.py
@@ -6,7 +6,7 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('state/', views.StateList.as_view(), name='state_list'),
-    path('state/<int:pk>', views.StateDetail.as_view(), name='state_detail'),
+    path('state/<int:pk>', views.StateDetail.as_view(), name='state-detail'),
     path('locations/', views.LocationList.as_view(), name='location_list'),
     path('locations/<int:pk>', views.LocationDetail.as_view(), name='location_detail'),
 ]
